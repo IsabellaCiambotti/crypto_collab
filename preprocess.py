a08301d131a68f28d4bb83535ec5b013e28710bc
Tyler Tarsi <tylertarsi@college.harvard.edu>
2018-07-03 15:06:27 +0200

    added featurize function2

--- preprocess.py
+++ preprocess.py
@@ -19,7 +19,7 @@ def read_trade_data(filepath):
     :param filepath: path to csv file
     :returns: (exchange, symbol, preprocessed Pandas dataframe)
     """
-    trades = pd.read_parquet(filepath)
+    trades = pd.read_csv(filepath)
     # convert dates from unix timestamps
     trades['date'] = trades['date'].apply(convert_dates)
     trades.set_index('date', inplace=True)
@@ -47,18 +47,19 @@ def write_processed(exchange, symbol, data, loc=None):
     filename = f'{exchange}_{symbol}_trades.parquet'
     path = os.getcwd() if loc is None else loc
     filepath = os.path.join(path, filename)
-    new_data = featurize(data)
-    new_data.to_parquet(filepath)
+    data.to_parquet(filepath)
 
 
-def featurize(trade_data):
+def featurize(filename):
     """
-    Takes trade data, adds features, and outputs the new dataframe.
+    Takes a parquet file and converts it to a pandas DataFrame to 
+    be outputted.
 
-    :param filename: data frame of trade data
+    :param filename: str parquet file to be read and processed
     """
-    # trade_data = pd.read_parquet(filename) 
-    trade_df = pd.DataFrame(trade_data) 
+    trade_data = pd.read_parquet(filename) 
+    trade_df = pd.DataFrame(trade_data)
+    trade_df["date"] = trade_df["MTS"].apply(lambda x: datetime.fromtimestamp(x / 1000))
     
     # Simple Moving Averages with differing windows
     trade_df["SMA5"] = trade_df["price"].rolling(window=5).mean()
@@ -68,14 +69,15 @@ def featurize(trade_data):
     trade_df["SMA100"] = trade_df["price"].rolling(window=100).mean()
 
     # Exponentially weighted mean
-    trade_df["ewm"] = trade_df["price"].ewm(com=0.5).mean()
+    trade_df["ewm_com05"] = trade_df["price"].ewm(com=0.5).mean()
+    trade_df["ewm_com1"] = trade_df["price"].ewm(com=1).mean()
 
     # Fast Fourier Transform
-    trade_df["fast_fourier"] = np.fft.fft(trade_df["price"])
-    
+    trade_df["fast_fourier"] = np.fft.fft(trade_df["price"]) 
     return trade_df
 
 if __name__ == "__main__":
     exchange, symbol, data = read_trade_data('Bitfinex_BTCEUR_trades_'
                                              '2018_02_02.csv')
     write_processed(exchange, symbol, data)
+
